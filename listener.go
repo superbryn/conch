1629ac3f954830efdc5290340d47065df6d1b387
Superbryn <superbryn@proton.me>
2026-04-10 22:26:05 +0530

    yoyo

--- listener.go
+++ listener.go
@@ -2,33 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/superbryn/conch/utils"
 	"net"
 	"time"
-	"github.com/superbryn/conch/utils"
 )
 
-type DiscoveryPackage struct{
+type DiscoveryPackage struct {
 	Username string `json:"username"`
-	IP string `json:"ip"`
-	Payload string `json:"payload"`
+	IP       string `json:"ip"`
+	Payload  string `json:"payload"`
 }
 
-func main(){
-	dest, _ := net.ResolveUDPAddr("udp4","255.255.255.255:6767")
-	conn, _ := net.DialUDP("udp4",nil,dest)
+func main() {
+	dest, _ := net.ResolveUDPAddr("udp4", "255.255.255.255:6767")
+	conn, _ := net.DialUDP("udp4", nil, dest)
 	ip_address := utils.IpAddrsFinder()
-	
+
 	packet := DiscoveryPackage{
 		Username: "Neeraj",
-		IP: ip_address,
-		Payload: "Conch Discovery",
+		IP:       ip_address,
+		Payload:  "Conch Discovery",
 	}
-	
+
 	for {
 		jsonData, _ := json.Marshal(packet)
-		
+
 		conn.Write(jsonData)
-		
+
 		time.Sleep(5 * time.Second)
 	}
 }
